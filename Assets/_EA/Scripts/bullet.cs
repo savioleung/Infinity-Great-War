05154beeb379711e01730e0e8fc9acde973c1ca2
savioleung <savioleung114514@yahoo.co.jp>
2022-01-22 14:12:53 +0900

    delete useless

---
+++ Assets/_EA/Scripts/bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+	//public GameObject gun;
+	Vector2 v;
+	Vector3 target;
+	Rigidbody2D body;
+	public float speed=10,atk=10;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+		//Vector3 norTar = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+//		Vector3 norTar = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+//		float angle = Mathf.Atan2 (norTar.y, norTar.x) * Mathf.Rad2Deg;
+////
+//		Quaternion rotation = new Quaternion ();
+//		rotation.eulerAngles = new Vector3 (0, 0, angle-90);
+//		transform.rotation = rotation;
+//		v.x= Mathf.Cos(Mathf.Deg2Rad * angle)*speed;
+//		v.y= Mathf.Sin(Mathf.Deg2Rad * angle)*speed;
+		body.velocity = transform.up * speed;
+	}
+//	void Update(){
+//		body.AddForce (gun.transform.forward * speed);
+//		//transform.position = new Vector3(transform.position.x+0.01f,transform.position.y,transform.position.z );
+//	}
+	
+
+}
---
+++ Assets/_EA/Scripts/mygun.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mygun : MonoBehaviour
+{
+    public GameObject bullet, bp, bp2;
+    public float speed = 1.5f, cd = 0.1f, cdd;
+    private Vector3 target;
+
+    void Start()
+    {
+        target = transform.position;
+        cdd = 0;
+    }
+
+    void Update()
+    {
+        gunControl();
+
+    }
+    void gunControl()
+    {
+        cdd += 1 * Time.deltaTime;
+        if (cdd >= cd)
+        {
+            cdd = 0;
+            GameObject clone = Instantiate(bullet, new Vector3(bp.transform.position.x, bp.transform.position.y, bp.transform.position.z - 10), transform.rotation) as GameObject;
+
+            //clone.transform.parent = this.transform;
+            Destroy(clone, 3);
+            GameObject clone2 = Instantiate(bullet, new Vector3(bp2.transform.position.x, bp2.transform.position.y, bp2.transform.position.z - 10), transform.rotation) as GameObject;
+            Destroy(clone2, 3);
+        }
+#if UNITY_EDITOR
+        Vector3 norTar = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+        float angle = Mathf.Atan2(norTar.y, norTar.x) * Mathf.Rad2Deg;
+
+        Quaternion rotation = new Quaternion();
+        rotation.eulerAngles = new Vector3(0, 0, angle - 90);
+        transform.rotation = rotation;
+#else
+		Touch myTouch = Input.GetTouch(0);
+		Touch[] myTouches = Input.touches;
+		for (int i = 0; i < Input.touchCount; i++) {
+			if (Input.touchCount <= 1|| this.tag=="gun") {
+				if (Input.touches [0].phase == TouchPhase.Began || Input.touches [0].phase == TouchPhase.Moved) {
+					Vector2 norTar = (Camera.main.ScreenToWorldPoint (Input.touches [0].position) - transform.position).normalized;
+					float angle = Mathf.Atan2 (norTar.y, norTar.x) * Mathf.Rad2Deg;
+
+					Quaternion rotation = new Quaternion ();
+					rotation.eulerAngles = new Vector3 (0, 0, angle - 90);
+					transform.rotation = rotation;
+				}
+			}
+			if (this.tag == "gun2") {
+				if (Input.touches [1].phase == TouchPhase.Began || Input.touches [1].phase == TouchPhase.Moved) {
+					Vector2 norTar = (Camera.main.ScreenToWorldPoint (Input.touches [1].position) - transform.position).normalized;
+					float angle = Mathf.Atan2 (norTar.y, norTar.x) * Mathf.Rad2Deg;
+
+					Quaternion rotation = new Quaternion ();
+					rotation.eulerAngles = new Vector3 (0, 0, angle - 90);
+					transform.rotation = rotation;
+				}
+			}
+			}
+#endif
+    }
+
+    //		Vector2 norTar = (Camera.main.ScreenToWorldPoint(Input.touches[0].position) - transform.position).normalized;
+
+
+
+
+    //if (Input.GetMouseButton(0)) {
+
+    //			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //			target.z = transform.position.z;
+    //			clone.transform.position = Vector3.MoveTowards(clone.transform.position, target, speed * Time.deltaTime);
+    //}
+
+
+}
+
